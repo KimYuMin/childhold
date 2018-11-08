a38c179e3e31073f8588da4735ad790080ea33b1
inyoungAum <inyoung.aum@ridi.com>
2018-11-09 01:38:04 +0900

    config kotlin, add child data model, make driver service

---
+++ app/src/main/java/com/example/yuminkim/childhold/model/Child.kt
@@ -0,0 +1,5 @@
+package com.example.yuminkim.childhold.model
+
+data class Child constructor(val idx: Int,
+                        val name: String,
+                        val beaconId: String)
--- app/src/main/java/com/example/yuminkim/childhold/network/DriverService.kt
+++ app/src/main/java/com/example/yuminkim/childhold/network/DriverService.kt
@@ -1,5 +1,11 @@
 package com.example.yuminkim.childhold.network
 
-class DriverService {
+import com.example.yuminkim.childhold.model.Child
+import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.Query
 
+interface DriverService {
+    @GET("child_list.php")
+    fun getChildList(@Query("driver_id") driverId: Int): Observable<Array<Child>>
 }
