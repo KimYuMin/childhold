5b7208d863bad08d810050d1489259dbe9ac5ad0
Umin <kimym9603@naver.com>
2018-11-15 20:01:26 +0900

    1115 작업

---
+++ app/src/main/java/com/example/yuminkim/childhold/activity/MapsActivity.java
@@ -0,0 +1,13 @@
+package com.example.yuminkim.childhold.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MapsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+    }
+}
---
+++ app/src/main/java/com/example/yuminkim/childhold/model/ChildListAdapter.java
@@ -0,0 +1,4 @@
+package com.example.yuminkim.childhold.model;
+
+public class ChildListAdapter {
+}
