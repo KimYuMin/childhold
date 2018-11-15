d78265798e6d155812a8f2ff5faa0c90ad2f4c55
inyoungAum <inyoung.aum@ridi.com>
2018-11-15 15:11:26 +0900

    push message 보내기 예제 // 나한테 보내기

--- app/src/main/java/com/example/yuminkim/childhold/activity/DriverActivity.java
+++ app/src/main/java/com/example/yuminkim/childhold/activity/DriverActivity.java
@@ -3,11 +3,17 @@ package com.example.yuminkim.childhold.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.example.yuminkim.childhold.R;
 import com.example.yuminkim.childhold.model.Child;
 import com.example.yuminkim.childhold.model.LatLng;
 import com.example.yuminkim.childhold.network.ApiService;
+import com.onesignal.OSPermissionSubscriptionState;
+import com.onesignal.OneSignal;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -27,6 +33,13 @@ public class DriverActivity extends Activity {
         setContentView(R.layout.activity_driver);
         getChildList(1);
         getDriverRoute(1);
+
+        findViewById(R.id.button_push).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendPushNotification();
+            }
+        });
     }
 
     private void getChildList(int driverId) {
@@ -65,6 +78,26 @@ public class DriverActivity extends Activity {
                         //TODO: handle error
                     }
                 });
+    }
+
+    private void sendPushNotification() {
+        OSPermissionSubscriptionState status = OneSignal.getPermissionSubscriptionState();
+        String userId = status.getSubscriptionStatus().getUserId();
+        boolean isSubscribed = status.getSubscriptionStatus().getSubscribed();
+
+        if (!isSubscribed)
+            return;
+
+        try {
+            JSONObject notificationContent = new JSONObject("{'contents': {'en': 'The notification message or body'}," +
+                    "'include_player_ids': ['" + userId + "'], " +
+                    "'headings': {'en': 'Notification Title'}, " +
+                    "'big_picture': 'http://i.imgur.com/DKw1J2F.gif'}");
+            OneSignal.postNotification(notificationContent, null);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
